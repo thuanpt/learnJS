ed4ca337dd994a017267cae66b7577a23682449d
thuanpt <thuanphong07@gmail.com>
2017-08-04 21:39:12 +0700

    add file

---
+++ ES6/destructuring.js
@@ -0,0 +1,17 @@
+// let arr = [1, "ES6", false];
+
+// let [id, name, free] = arr;
+
+// console.log(id + " -" + name + "-" + free);
+
+//Object
+
+let objCourse = {
+    id: 123,
+    name: "ES6",
+    free: false
+}
+
+let {id, name, free} = objCourse;
+
+console.log(id + " -" + name + "-" + free);
---
+++ ES6/rest-param.js
@@ -0,0 +1,7 @@
+function showInfo(name, ...price) {
+    console.log(price); 
+}
+
+showInfo(2,3,5,67);
+
+//rut ngan tham so truyen vao 1 function bang ...
